11e4f323f3b72204fa8f17f826115eae03cb9727
Jimmy Frey <jamessfreyiii@gmail.com>
2015-05-19 14:05:05 -0500

    Add expand/collapse imageviews and ids

--- app/src/main/java/frey/jimmy/recipe/recipselector/RecipeDisplayFragment.java
+++ app/src/main/java/frey/jimmy/recipe/recipselector/RecipeDisplayFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 
 public class RecipeDisplayFragment extends Fragment {
@@ -24,6 +25,8 @@ public class RecipeDisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_recipe_display, container, false);
+        ScrollView instructionsScrollView = (ScrollView) v.findViewById(R.id.instructionsScrollView);
+
 
 
         return v;
